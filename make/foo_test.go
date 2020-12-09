11e9018f650e0ae116a2aa4a872b6b41c82168c6
Robert Pajak <pellared@hotmail.com>
2020-12-09 12:52:51 +0100

    refine tests

--- make/foo_test.go
+++ make/foo_test.go
@@ -2,6 +2,9 @@ package foo
 
 import "testing"
 
-func Test(t *testing.T) {
-	t.Error("I am failing. Sorry")
+func TestFoo(t *testing.T) {
+	want := "Foo"
+	if got := Foo(); got != want {
+		t.Errorf("Foo() = %v, want %v", got, want)
+	}
 }
