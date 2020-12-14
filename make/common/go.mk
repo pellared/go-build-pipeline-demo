20ef8f012610c24683868983ba179a7854abf2bb
Robert Pajak <pellared@hotmail.com>
2020-12-14 11:57:20 +0100

    Refactor Makefiles

--- make/common/go.mk
+++ make/common/go.mk
@@ -2,6 +2,8 @@
 
 SHELL := /bin/bash
 
+.DEFAULT_GOAL := help
+
 .PHONY: help
 help:
 	@cat $(MAKEFILE_LIST) | grep -E '^[a-zA-Z_-]+:.*?## .*$$'  | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
